6d4248e657e227857398521a508521d5bcf62771
Dwake5 <wakeling101@hotmail.com>
2019-04-15 20:47:18 +0100

    Wrote up rudimentary first day

--- app/cli.rb
+++ app/cli.rb
@@ -4,10 +4,11 @@ class CLI
   def initialize
     @prompt = TTY::Prompt.new
     @user = nil
+    @occupation = nil
     @story = [
-      {
+      { #Start day 1
         id: 1,
-        description: 'You wake up in the morning, first day of school...',
+        description: "Good morning #{self.name}. You've just woken up for your first day at flatiron school. Do you want to get up early and have a healthy breakfest or go back to sleep?"
         choices: [
           { choice: 2, text: 'Hit snooze.'},
           { choice: 3, text: 'Get up.'},
@@ -15,27 +16,65 @@ class CLI
       },
       {
         id: 2,
-        description: "You're late!!!",
+        description: "You hit snooze a few too many times, and now need to rush into class",
         choices: [
-          { choice: 4, text: 'Take the train.'},
-          { choice: 4, text: 'Cycle.'},
-          { choice: 4, text: 'Run.'},
+          { choice: 5, text: 'Take the train.'},
+          { choice: 5, text: 'Cycle.'},
+          { choice: 5, text: 'Run.'},
         ],
       },
       {
         id: 3,
-        description: 'You wake up nice and early, and enjoy a healthy breakfast!',
+        description: "You wake up nice and early, and enjoy a healthy breakfast! You've got some spare time. How are you getting to school?",
         choices: [
-          { choice: 4, text: 'Walk'},
-          { choice: 4, text: 'Get the tube.'},
+          { choice: 5, text: 'Walk'},
+          { choice: 5, text: 'Get the tube.'},
         ]
       },
       {
-        id: 4,
-        description: 'You get to school!',
+        id: 5,
+        description: 'You arrive at school eager and ready to learn. You notice that everyone else is seated. Looking around people have split into two distinct groups, who do you want to sit with?',
         choices: [
-          { choice: 1, text: 'Nice'},
-          { choice: 1, text: 'Awesome!'},
+          { choice: 6, text: 'The nerds?'},
+          { choice: 7, text: 'The cool group!'},
+        ]
+      },
+      {
+        id: 6,
+        description: 'You sit with the nerds (You look like you fit right in), you mingle a bit and get straight to work. You hear labs are coming up soon, do you want a coffe first?',
+        choices: [
+          { choice: 8, text: 'Coffee, coffee, coffee!'},
+          { choice: 9, text: 'Straight to the labs'},
+        ]
+      },
+      {
+        id: 7,
+        description: 'You sit down in the cool group, they continue to laugh and joke along with you, you waste a few hours screwing around, but make some friends. You hear labs are coming up soon, do you want a coffe first?',
+        choices: [
+          { choice: 8, text: 'Coffee, coffee, coffee!'},
+          { choice: 9, text: 'Straight to the labs'},
+        ]
+      },
+      {
+        id: 8,
+        description: "The coffee here taste's great, better than that crap you get from 'popular coffee franchise, you know the one' you eagerly take on the labs",
+        choices: [
+          { choice: 9, text: 'Take on the labs!'},
+        ]
+      },
+      {
+        id: 9, #Kind of plain without a battle encounter
+        description: "You take on the labs, they've realised 81 on the first day. How is this fair?",
+        choices: [
+          { choice: 10, text: 'You move onto lunch'},
+        ]
+      },
+      {
+        id: 7,
+        description: 'You sit down in the cool group, they continue to laugh and joke along with you, you waste a few hours screwing around, but make some friends. You hear labs are coming up soon, do you want a coffe first?',
+        choices: [
+          { choice: 8, text: 'Coffee, coffee, coffee!'},
+          { choice: 9, text: 'Straight to the labs'},
         ]
       },
     ]
@@ -44,7 +83,8 @@ class CLI
 
   def create_user
     name = @prompt.ask("What's your name?")
-    @user = User.create(name: name)
+    occupation = @prompt.ask("What was your career before coding?")
+    @user = User.create(name: name, occupation: occupation)
   end
 
   def choose_scenario
@@ -63,18 +103,6 @@ class CLI
     @current_scenario = @story.find { |scenario| scenario[:id] == next_scenario_id }
     display_current_scenario
   end
-
-  # def show_options
-  # end
-  #
-  # def display_scenario
-  #   display_current_scenario
-  #   # how_options
-  # end
-  #
-  # def start
-  #   display_scenario
-  # end
 
   def run
     create_user
